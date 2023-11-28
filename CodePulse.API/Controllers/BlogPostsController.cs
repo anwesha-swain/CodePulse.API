c09a3c47af731d7f4a107c90673cb3e254f0a92d
anweshaswain-mcd <anwesha.swain@capgemini.com>
2023-11-28 17:45:04 +0530

    Add new blog post

---
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,55 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+
+        //POST: {apibaseurl}/api/blogposts
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            //convert dto to domain
+            var blogPost = new BlogPost
+            {
+                Author = request.Author,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                PublishedDate = request.PublishedDate,
+                ShortDescription = request.ShortDescription,
+                Title = request.Title,
+                UrlHandle = request.UrlHandle
+            };
+
+            blogPost = await blogPostRepository.CreateAsync(blogPost);   //create details in repo
+
+            //convert domain model back to dto
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishedDate = blogPost.PublishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle
+            };
+
+            return Ok();
+        }
+    }
+}
